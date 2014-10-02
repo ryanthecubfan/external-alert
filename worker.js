2ac7b8b2c17c907b0e879e128d8da637b953f92d
Ryan Brewer <ryan.brewer@gettyimages.com>
2014-10-02 14:12:54 -0700

    fixed promises and improved error handling

--- worker.js
+++ worker.js
@@ -2,34 +2,37 @@ var Scheduler     = require('./lib/scheduler.js'),
     HttpLoader    = require('./lib/httpLoader.js'),
     PreviewParser = require('./lib/parsePreviewData.js'),
     FrameParser   = require('./lib/parseIframe.js'),
-    Mailer		  = require('./lib/mailer.js');
+    Mailer		  = require('./lib/mailer.js'),
+    Q			  = require('q');
 
 process.on('uncaughtException', function (err) {
-    fail(err);
+	console.log("****************Uncaught Exception****************");
+	console.log(err);
+	Mailer.sendMail("Embed Preview Failure", "Uncaught Exception: \r\n\r\nReason:\r\n\r\n" + message, mailRecipients);
 }); 
 
 var mailRecipients = process.env.MAIL_RECIPIENTS || "ryan.brewer@gettyimages.com";
 var previewUrl = process.env.PREVIEW_URL || "http://embed.gettyimages.com/preview/1765189";
 var previewTimer = process.env.PREVIEW_TIMER || 60000;
 
-var fail = function(message) {
-	console.log(message);
-	Mailer.sendMail("Embed Preview Failure", "Embed Preview failed: \r\n\r\nReason:\r\n\r\n" + message, mailRecipients);
-}
 
 var scheduler = new Scheduler();
 
-scheduler.addTask(function() {
+scheduler.addTask(function previewLoaderExecute(previewUrl) {
 	console.log("Executing preview loader task.")
-	HttpLoader.execute(previewUrl)
-		.then(PreviewParser.execute, fail)
-		.then(HttpLoader.execute, fail)
-		.then(FrameParser.execute, fail)
-		.then(previewLoaderSuccess, fail);
-	}, previewTimer);
+	var p = HttpLoader.execute(previewUrl)
+	    .then(PreviewParser.execute)
+		.then(HttpLoader.execute)
+		.then(FrameParser.execute)
+		.then(HttpLoader.execute)
+		.then(function(data) {
+			console.log("Successfully created embed via '" + previewUrl + "', viewed the iframe, and loaded the image.")
+		})
+		.catch(function(message) {
+			console.log("****************Loader Exception****************");
+			console.log(message);
+			Mailer.sendMail("Embed Preview Failure", "Embed Preview failed: \r\n\r\nReason:\r\n\r\n" + message, mailRecipients);
+		});
+}, previewTimer);
 
 scheduler.start();
-
-function previewLoaderSuccess(data) {
-	console.log("Successfully created embed via '" + previewUrl + "', viewed the iframe, and loaded the image.")
-}
