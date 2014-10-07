020558f164917eac472cce501ec9f14e7b266220
Ryan Brewer <ryan.brewer@gettyimages.com>
2014-10-07 10:24:02 -0700

    Added multiple environment configs, made logging and mailing more robust

--- worker.js
+++ worker.js
@@ -1,24 +1,29 @@
-var EmbedWF       = require('./lib/workflows/embed.js'),
+var config		  = require('./lib/config/config.js'),
+	EmbedWF       = require('./lib/workflows/embed.js'),
     EmbedCdnWF    = require('./lib/workflows/cdn.js'),
     EmbedSearchWF = require('./lib/workflows/search.js'),
     EmbedMltWF    = require('./lib/workflows/mlt.js'),
     AlertManager  = require('./lib/alertManager.js'),
-    Scheduler     = require('./lib/scheduler.js'),
-    Q			  = require('q');
+    Scheduler     = require('./lib/scheduler.js');
 
-var mailRecipients = process.env.MAIL_RECIPIENTS || "ryan.brewer@gettyimages.com";
+var mailRecipients = "ryan.brewer@gettyimages.com";
 
 process.on('uncaughtException', function (err) {
-	console.log("****************Uncaught Exception****************");
-	console.log(err);
-	Mailer.sendMail("Embed Preview Failure", "Uncaught Exception: \r\n\r\nReason:\r\n\r\n" + err, mailRecipients);
+	log("***UNCAUGHT EXCEPTION***");
+	log(err);
+	AlertManager.sendMail(config.mailer.defaultSubject, "Uncaught Exception: \r\n\r\nReason:\r\n\r\n" + err, mailRecipients);
 }); 
+
 
 var scheduler = new Scheduler();
 
-scheduler.addTask(EmbedWF.createWorkflow());
-scheduler.addTask(EmbedCdnWF.createWorkflow());
-scheduler.addTask(EmbedSearchWF.createWorkflow());
-scheduler.addTask(EmbedMltWF.createWorkflow());
+scheduler.addWorkflow(EmbedWF.createWorkflow());
+scheduler.addWorkflow(EmbedCdnWF.createWorkflow());
+scheduler.addWorkflow(EmbedSearchWF.createWorkflow());
+scheduler.addWorkflow(EmbedMltWF.createWorkflow());
 
 scheduler.start();
+
+function log(msg) {
+	console.log("Level=INFO\tComponent=WORKER\t" + msg);
+}
