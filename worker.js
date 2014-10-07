75af34683829a0397fcfac04d427f27866beca76
Ryan Brewer <ryan.brewer@gettyimages.com>
2014-10-07 10:34:13 -0700

    fixed case inconsistency

--- worker.js
+++ worker.js
@@ -1,9 +1,9 @@
 var config		  = require('./lib/config/config.js'),
-	EmbedWF       = require('./lib/workflows/embed.js'),
-    EmbedCdnWF    = require('./lib/workflows/cdn.js'),
-    EmbedSearchWF = require('./lib/workflows/search.js'),
-    EmbedMltWF    = require('./lib/workflows/mlt.js'),
-    AlertManager  = require('./lib/alertManager.js'),
+	embedWF       = require('./lib/workflows/embed.js'),
+    embedCdnWF    = require('./lib/workflows/cdn.js'),
+    embedSearchWF = require('./lib/workflows/search.js'),
+    embedMltWF    = require('./lib/workflows/mlt.js'),
+    alertManager  = require('./lib/alertManager.js'),
     Scheduler     = require('./lib/scheduler.js');
 
 var mailRecipients = "ryan.brewer@gettyimages.com";
@@ -11,16 +11,16 @@ var mailRecipients = "ryan.brewer@gettyimages.com";
 process.on('uncaughtException', function (err) {
 	log("***UNCAUGHT EXCEPTION***");
 	log(err);
-	AlertManager.sendMail(config.mailer.defaultSubject, "Uncaught Exception: \r\n\r\nReason:\r\n\r\n" + err, mailRecipients);
+	alertManager.sendMail(config.mailer.defaultSubject, "Uncaught Exception: \r\n\r\nReason:\r\n\r\n" + err, mailRecipients);
 }); 
 
 
 var scheduler = new Scheduler();
 
-scheduler.addWorkflow(EmbedWF.createWorkflow());
-scheduler.addWorkflow(EmbedCdnWF.createWorkflow());
-scheduler.addWorkflow(EmbedSearchWF.createWorkflow());
-scheduler.addWorkflow(EmbedMltWF.createWorkflow());
+scheduler.addWorkflow(embedWF.createWorkflow());
+scheduler.addWorkflow(embedCdnWF.createWorkflow());
+scheduler.addWorkflow(embedSearchWF.createWorkflow());
+scheduler.addWorkflow(embedMltWF.createWorkflow());
 
 scheduler.start();
 
