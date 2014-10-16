1bf9f525405bfc48de027f7d144f34ad13c669e5
jspeaker <jspeaker@gettyimages.com>
2014-10-16 16:37:38 -0700

    Refactored logging to use common log.js file

--- worker.js
+++ worker.js
@@ -4,13 +4,15 @@ var config		  = require('./lib/config/config.js'),
     embedSearchWF = require('./lib/workflows/search.js'),
     embedMltWF    = require('./lib/workflows/mlt.js'),
     alertManager  = require('./lib/alertManager.js'),
-    Scheduler     = require('./lib/scheduler.js');
+    Scheduler     = require('./lib/scheduler.js'),
+    log  		  = require('./lib/log.js');
 
 var mailRecipients = "ryan.brewer@gettyimages.com";
+var component      = "WORKER";
 
 process.on('uncaughtException', function (err) {
-	log("***UNCAUGHT EXCEPTION***");
-	log(err);
+	log.write(component, "***UNCAUGHT EXCEPTION***");
+	log.write(component, err);
 	alertManager.sendMail(config.mailer.defaultSubject, "Uncaught Exception: \r\n\r\nReason:\r\n\r\n" + err, mailRecipients);
 }); 
 
@@ -23,7 +25,3 @@ scheduler.addWorkflow(embedSearchWF.createWorkflow());
 scheduler.addWorkflow(embedMltWF.createWorkflow());
 
 scheduler.start();
-
-function log(msg) {
-	console.log("Level=INFO\tComponent=WORKER\t" + msg);
-}
