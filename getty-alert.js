97234fa495a1487107f9854ef616a24952939a27
Ryan Brewer <ryan.brewer@gettyimages.com>
2014-10-23 14:07:19 -0700

    missing exported function

--- getty-alert.js
+++ getty-alert.js
@@ -5,6 +5,7 @@
 exports = module.exports = {
 	Scheduler      : require('./lib/scheduler.js'),
 	Log            : require('./lib/log.js'),
+	UrlRetriever   : require('./lib/UrlRetriever.js')
 	AlertManager   : require('./lib/alertManager.js'),
 	FailureTracker : require('./lib/failureTracker.js'),
 	Workflow       : require('./lib/workflow.js')
