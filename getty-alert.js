d7bc6e885af9ecb506910750a9796da706d04409
Ryan Brewer <ryan.brewer@gettyimages.com>
2014-10-23 14:13:29 -0700

    more cleaning up alert code

--- getty-alert.js
+++ getty-alert.js
@@ -6,7 +6,7 @@ exports = module.exports = {
 	Scheduler      : require('./lib/scheduler.js'),
 	Log            : require('./lib/log.js'),
 	UrlRetriever   : require('./lib/UrlRetriever.js')
-	AlertManager   : require('./lib/alertManager.js'),
+	Mailer         : require('./lib/mailer.js'),
 	FailureTracker : require('./lib/failureTracker.js'),
 	Workflow       : require('./lib/workflow.js')
 }
