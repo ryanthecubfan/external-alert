d48b2463cba5324aaed7a07046bf82354a0c5a11
Ryan Brewer <ryan.brewer@gettyimages.com>
2014-11-06 14:24:34 -0800

    Fixing case bug

--- getty-alert.js
+++ getty-alert.js
@@ -5,7 +5,7 @@
 exports = module.exports = {
 	Scheduler      : require('./lib/scheduler.js'),
 	Log            : require('./lib/log.js'),
-	UrlRetriever   : require('./lib/UrlRetriever.js'),
+	UrlRetriever   : require('./lib/urlRetriever.js'),
 	Mailer         : require('./lib/mailer.js'),
 	FailureTracker : require('./lib/failureTracker.js'),
 	Workflow       : require('./lib/workflow.js')
